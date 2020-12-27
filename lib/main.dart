7836b16eba00a5f96c44f05df4ea1b71abda5a0c
Haggerman <jan.ther@seznam.cz>
2020-12-27 15:46:14 +0100

    Změna gest

--- lib/main.dart
+++ lib/main.dart
@@ -108,29 +108,58 @@ class _CanvasPaintingState extends State<CanvasPainting> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body:RepaintBoundary(
-          key: globalKey,
-          child: Stack(
-            children: <Widget>[
-              Center(
-                child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: _image == null
-                            ? MemoryImage(kTransparentImage)
-                            : FileImage(_image),
-                      ),
-                    )
+        body:GestureDetector(
+          onPanUpdate: (details) {
+            setState(() {
+              RenderBox renderBox = context.findRenderObject();
+              points.add(TouchPoints(
+                  points: renderBox.globalToLocal(details.globalPosition),
+                  paint: Paint()
+                    ..strokeCap = strokeType
+                    ..isAntiAlias = true
+                    ..color = selectedColor.withOpacity(opacity)
+                    ..strokeWidth = strokeWidth));
+            });
+          },
+          onPanStart: (details) {
+            setState(() {
+              RenderBox renderBox = context.findRenderObject();
+              points.add(TouchPoints(
+                  points: renderBox.globalToLocal(details.globalPosition),
+                  paint: Paint()
+                    ..strokeCap = strokeType
+                    ..isAntiAlias = true
+                    ..color = selectedColor.withOpacity(opacity)
+                    ..strokeWidth = strokeWidth));
+            });
+          },
+          onPanEnd: (details) {
+            setState(() {
+              points.add(null);
+            });
+          },
+          child: RepaintBoundary(
+            key: globalKey,
+            child: Stack(
+              children: <Widget>[
+                Center(
+                  child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: _image == null
+                              ? MemoryImage(kTransparentImage)
+                              : FileImage(_image),
+                        ),
+                      )),
                 ),
-              ),
-              CustomPaint(
-                size: Size.infinite,
-                painter: MyPainter(
-                  pointsList: points,
+                CustomPaint(
+                  size: Size.infinite,
+                  painter: MyPainter(
+                    pointsList: points,
+                  ),
                 ),
-              ),
-
-            ],
+              ],
+            ),
           ),
         ),
         floatingActionButton: Column(
