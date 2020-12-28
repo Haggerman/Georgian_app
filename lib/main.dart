588f301727720c4ee4d19d20781a0fb3ae4799d5
Haggerman <jan.ther@seznam.cz>
2020-12-28 20:51:33 +0100

    Zmena velikosti obrazku

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/services.dart';
 
 
+
 void main() {
   runApp(MaterialApp(home: CanvasPainting()));
 }
@@ -23,30 +24,38 @@ class CanvasPainting extends StatefulWidget {
 
 class _CanvasPaintingState extends State<CanvasPainting> {
   GlobalKey globalKey = GlobalKey();
-
   List<TouchPoints> points = List();
   File _image;
   int height = 0;
   int width = 0;
+  double widthScreen;
   GlobalKey _keyImage = GlobalKey();
   String text = 'Loading...';
   StateSetter _setState;
+  String coordinates ="";
   final picker = ImagePicker();
 
 
-
   _showAlertDialog(BuildContext context) {
+
+    var pressed = false ;
     showDialog(
       context: context,
+      barrierColor: Colors.white.withOpacity(0.1),
       builder: (BuildContext context) {
 
           return StatefulBuilder(  // You need this, notice the parameters below:
             builder: (BuildContext context, StateSetter setState) {
               _setState = setState;
               return AlertDialog (
-                title: Center(child: Text(text),),
-                actions: <Widget>[
-                  FlatButton(onPressed: (){Clipboard.setData(new ClipboardData(text:text));}, child: Text("Zkopírovat"))
+                backgroundColor: Colors.black.withOpacity(0.7),
+                title: Center(child: Text(text, style: TextStyle(color: Colors.lightBlue)),),
+                actions:  <Widget>[
+                  FlatButton(onPressed: pressed? null: (){Clipboard.setData(new ClipboardData(text:text));
+                  setState((){
+                    pressed = !pressed ; // update the state of the class to show color change
+                  });
+                  }, child: pressed? null : Text("Zkopírovat"),color: pressed ? Colors.transparent : Colors.green,)
                 ],
               );
             },
@@ -55,15 +64,38 @@ class _CanvasPaintingState extends State<CanvasPainting> {
     );
   }
 
+  String _coordinates(){
+    for (int i = 0; i < points.length-1; i=i+2) {
+      int x1 = (points[i].points.dx).toInt();
+      int x2 = (points[i+1].points.dx).toInt();
+      int y1 = (points[i].points.dy).toInt();
+      int y2 = (points[i+1].points.dy).toInt();
+      int odklad;
+      if(x2<x1){
+        odklad = x1;
+        x1 = x2;
+        x2 = odklad;
+      }
+      if(y2<y1){
+        odklad = y1;
+        y1 = y2;
+        y2 = odklad;
+      }
+
+      coordinates = coordinates + x1.toString() + "," + y1.toString() + "," + x2.toString() + "," + y2.toString() + "," ;
+    }
+    coordinates = coordinates.substring(0, coordinates.length - 1);
+    return coordinates;
+  }
+
   Future _getText() async {
     text="Loading...";
-    String coordinates ="";
+    coordinates = "";
+
+
     if(points.length > 0) {
-      coordinates = "";
-      for (int i = 0; i < points.length-1; i++) {
-        coordinates = coordinates + ((points[i].points.dx).toInt()).toString() + "," + ((points[i].points.dy).toInt()).toString() + "," ;
-      }
-      coordinates = coordinates + ((points[points.length-1].points.dx).toInt()).toString() + "," + ((points[points.length-1].points.dy).toInt()).toString();
+     coordinates = _coordinates();
+     print(coordinates);
     }
     var request = http.MultipartRequest(
         "POST",
@@ -79,7 +111,13 @@ class _CanvasPaintingState extends State<CanvasPainting> {
     request.send().then((result) async {
       http.Response.fromStream(result)
           .then((response) {
-        if (response.statusCode == 200)
+        if (response.statusCode == 404)
+        {
+          _setState(() {
+            text = "Chyba spojení";
+          });
+        }
+        else if (response.statusCode == 200)
         {
           Map mapJson = json.decode(utf8.decode(response.bodyBytes));
           print(mapJson["text"]);
@@ -99,15 +137,19 @@ class _CanvasPaintingState extends State<CanvasPainting> {
   }
 
   Future _getImage() async {
-    final pickedFile = await picker.getImage(source: ImageSource.gallery, maxHeight: 650);
+    double widthScreen = MediaQuery.of(context).size.width;
+    final pickedFile = await picker.getImage(source: ImageSource.gallery, maxWidth: widthScreen);
     if (pickedFile != null) {
       _image = File(pickedFile.path);
       var decodedImage = await decodeImageFromList(_image.readAsBytesSync());
       width= decodedImage.width;
       height= decodedImage.height;
+      print(width);
+      print(height);
     }
     setState(() {
       if (pickedFile != null) {
+        points.clear();
         _image = File(pickedFile.path);
         print('_image: $_image');
       } else {
@@ -119,9 +161,13 @@ class _CanvasPaintingState extends State<CanvasPainting> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.white70,
         body: RepaintBoundary(
             key: globalKey,
             child: Center(
@@ -131,6 +177,7 @@ class _CanvasPaintingState extends State<CanvasPainting> {
                 height: height.toDouble(),
                 decoration: BoxDecoration(
                   image: DecorationImage(
+
                     image: _image == null
                         ? MemoryImage(kTransparentImage)
                         : FileImage(_image),
@@ -160,7 +207,7 @@ class _CanvasPaintingState extends State<CanvasPainting> {
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               FloatingActionButton(
-                backgroundColor: Colors.indigo,
+                backgroundColor: Colors.green,
                 onPressed: () async{await _getImage();},
                 tooltip: 'Vybrat obrazek',
                 child: Icon(Icons.add),
@@ -170,7 +217,7 @@ class _CanvasPaintingState extends State<CanvasPainting> {
                   heroTag: "erase",
                   child: Icon(Icons.clear),
                   tooltip: "Erase",
-                  backgroundColor: points.length == 0 ? Colors.blueGrey.withOpacity(0.5): Colors.indigo,
+                  backgroundColor: points.length == 0 ? Colors.blueGrey.withOpacity(0.5): Colors.green,
                   onPressed: points.length == 0 ? null : () {
                     setState(() {
                       points.clear();
@@ -178,7 +225,19 @@ class _CanvasPaintingState extends State<CanvasPainting> {
                   }),
               SizedBox(height: 5),
               FloatingActionButton(
-                backgroundColor: _image == null ? Colors.blueGrey.withOpacity(0.5): Colors.indigo,
+                  heroTag: "stepBack",
+                  child: Icon(Icons.keyboard_backspace),
+                  tooltip: "Step back",
+                  backgroundColor: points.length < 2 ? Colors.blueGrey.withOpacity(0.5): Colors.green,
+                  onPressed: points.length < 2 ? null : () {
+                    setState(() {
+                      points.removeLast();
+                      points.removeLast();
+                    });
+                  }),
+              SizedBox(height: 5),
+              FloatingActionButton(
+                backgroundColor: _image == null ? Colors.blueGrey.withOpacity(0.5): Colors.green,
                 onPressed: _image == null
                     ? null
                     : () async { await _getText();
@@ -205,6 +264,7 @@ class MyPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     for (int i = 0; i < pointsList.length - 1; i= i+2) {
       print(pointsList[i].points.dx);print(pointsList[i].points.dy);
+      print(pointsList[i+1].points.dx);print(pointsList[i+1].points.dy);
       if (pointsList[i] != null && pointsList[i + 1] != null) {
         //Drawing line when two consecutive points are available
         canvas.drawRect(
